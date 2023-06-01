703902c23884349558c5312e281e0e00efa610dc
hackerd25 <dst.site.25@gmail.com>
2023-06-01 11:28:35 -0500

    Added ip choosing(2# commit)

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-
+import socket as sock
+def ip():
+    ip = input("Local computer ip(It is going to crash if it doesnt work): ")
+    return ip
+FTP_HOST = ip()
 FTP_PORT = 2121
 
 FTP_USER = "hackerd25"
-
 FTP_PASSWORD = "2"
-
-FTP_DIRECTORY = "/home/hackerd25/Documents/Visual Studio Code Projects/test"
-
+FTP_DIRECTORY = "/home/hackerd25/Documents/Visual Studio Code Projects/ftp-er/fdghfkd"
 
 def main():
     authorizer = DummyAuthorizer()
@@ -18,17 +19,15 @@ def main():
     handler = FTPHandler
     handler.authorizer = authorizer
 
-
     handler.banner = "Lets start pwning!"
 
-    address = ('', FTP_PORT)
+    address = (FTP_HOST, FTP_PORT)
     server = FTPServer(address, handler)
 
     server.max_cons = 256
     server.max_cons_per_ip = 5
 
     server.serve_forever()
-
 
 if __name__ == '__main__':
     main()
