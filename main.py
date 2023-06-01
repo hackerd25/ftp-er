c5d3ff0f9551d537d7c1c7619885eda30fbdba21
hackerd25 <dst.site.25@gmail.com>
2023-06-01 11:46:25 -0500

    Added port choosing(3# commit)

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-import socket as sock
 def ip():
     ip = input("Local computer ip(It is going to crash if it doesnt work): ")
     return ip
+def portchecker():
+    port = int(input("Give a free ip higher than 1023(If you dont know just write 2121): "))
+    if port < 1023 or port > 65535:
+        print("Not in the limits.Try again: ")
+        portchecker()
+    return port
 FTP_HOST = ip()
-FTP_PORT = 2121
+FTP_PORT = portchecker()
 
 FTP_USER = "hackerd25"
 FTP_PASSWORD = "2"
-FTP_DIRECTORY = "/home/hackerd25/Documents/Visual Studio Code Projects/ftp-er/fdghfkd"
+FTP_DIRECTORY = "/home/hackerd25/Documents/Visual Studio Code Projects/ftp-er/test"
 
 def main():
     authorizer = DummyAuthorizer()
